b542d03d2c0581905863f4ac09360c407c59b507
Neo Chen <43288434+NeoKokuxz@users.noreply.github.com>
2023-06-25 00:41:04 -0400

    Update 0088. Merge Sorted Array.js

--- 0088. Merge Sorted Array.js
+++ 0088. Merge Sorted Array.js
@@ -5,6 +5,8 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
+
+// 2 pointers
 var merge = function(nums1, m, nums2, n) {
     // Set pointers to the end of both array
     let p1 = m - 1; // Last position of the nums1
@@ -22,4 +24,22 @@ var merge = function(nums1, m, nums2, n) {
         }
         lp--
     }
+};
+
+// Sort 
+var merge = function(nums1, m, nums2, n) {
+    if(m == 0 ) {
+        for(let i = 0; i < n; i ++) {
+            nums1[i] = nums2[i]
+        }
+    }
+
+    let x = 0;
+    if(n != 0) {
+        for(let i = m; i < m + n; i++) {
+            nums1[i] = nums2[x]
+            x++;
+        }
+    }
+    nums1.sort(function(a,b){return a - b})
 };
