d4aeba2607264e43d6c345babd13fb68b635de42
Neo Chen <43288434+NeoKokuxz@users.noreply.github.com>
2023-07-03 21:16:22 -0400

    Update 0001. Two Sum.js

--- 0001. Two Sum.js
+++ 0001. Two Sum.js
@@ -3,6 +3,7 @@
  * @param {number} target
  * @return {number[]}
  */
+// O(n)
 var twoSum = function(nums, target) {
     const numMap = new Map();
     for( n in nums ) {
@@ -14,4 +15,19 @@ var twoSum = function(nums, target) {
             numMap.set(curr, n)
         }
     }
+};
+
+// O(n^2)
+var twoSum = function(nums, target) {
+    let res = []
+    for(var i = 0; i < nums.length; i++) {
+        let lookFor = target - nums[i];
+        for(var j = i + 1; j < nums.length; j++) {
+            if(lookFor == nums[j]) {
+                res.push(i);
+                res.push(j);
+            }
+        }
+    }
+    return res;
 };
